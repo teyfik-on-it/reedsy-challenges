6fda210766e9108a3fd32d37eb5a364c64dad019
Halil Teyfik <hlltvfk@gmail.com>
2020-11-19 17:56:45 +0300

    created card component

---
+++ front-end/q4/client/src/components/Card.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card p-2 elevation-z4 bg-white">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+};
+</script>
+
+<style scoped></style>
